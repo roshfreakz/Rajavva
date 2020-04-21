18696904fd17c12ed84623b88678101ad17fd6e1
roshfreakz <roshabinash66@gmail.com>
2020-04-21 13:17:19 +0530

    3.0

--- js/app.js
+++ js/app.js
@@ -1,25 +1,5 @@
 
- // loader
- var loader = function () {
-  setTimeout(function () {
-    if ($('#ftco-loader').length > 0) {
-       $('#ftco-loader').removeClass('show');
-    }
-  }, 1000);
-};
-loader();
 
-mybutton = document.getElementById("gototop");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    mybutton.style.display = "flex";  
-  } else {
-    mybutton.style.display = "none";
-  }
-}
 
 
 
